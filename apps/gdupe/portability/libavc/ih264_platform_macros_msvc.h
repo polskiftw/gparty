9e28aebf06e12ef5552ead2a09b731537cddcade
polskiftw <polskiftw@gmail.com>
2026-08-15 05:35:43 -0400

    Mark libavc MSVC adaptation under upstream license

--- apps/gdupe/portability/libavc/ih264_platform_macros_msvc.h
+++ apps/gdupe/portability/libavc/ih264_platform_macros_msvc.h
@@ -1,6 +1,24 @@
-/*
- * Windows/MSVC x64 adaptation of AOSP libavc's x86 platform macro layer.
+/******************************************************************************
  *
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Originally developed and contributed by Ittiam Systems Pvt. Ltd, Bangalore
+ ******************************************************************************/
+/*
+ * Modified for gdupe on 2026-08-15.
+ * Windows/MSVC x64 adaptation of AOSP libavc's x86 platform macro layer.
  * This file replaces compiler/ABI glue only for the gdupe static decoder
  * build. H.264 codec logic remains at the pinned upstream commit.
  */
